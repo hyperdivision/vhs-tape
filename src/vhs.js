fb41b7be5dbb518f13a601e2caf5d2341f52e17d
Mathias Buus <mathiasbuus@gmail.com>
2019-07-23 14:46:14 +0200

    more helpers and add vhs.delay()

--- src/vhs.js
+++ src/vhs.js
@@ -5,49 +5,77 @@ const onload = require('fast-on-load')
 const testBody = html`<div id="vhs-test-body"></div>`
 document.body.appendChild(testBody)
 
-function queueTest (description, testFn) {
-  tape(description, t => {
-    const testElementGroup = html`<div>
-      <div>${description}</div>
-        <div class="test-element"></div>
-      </div>`
-    const testElement = testElementGroup.querySelector('.test-element')
+function create (delay) {
+  function queueTest (description, testFn) {
+    tape(description, t => {
+      const testElementGroup = html`<div>
+        <div>${description}</div>
+          <div class="test-element"></div>
+        </div>`
+      const testElement = testElementGroup.querySelector('.test-element')
 
-    testBody.appendChild(testElementGroup)
-    const maybePromise = testFn(createTestHarness(t, testElement))
-    if (maybePromise && maybePromise.then) {
-      maybePromise.then(t.end).catch(t.error)
-    }
-  })
-}
-
-module.exports = queueTest
-
-function createTestHarness (t, element) {
-  return Object.assign(t, {
-    element,
-    sleep: ms => new Promise((resolve) => setTimeout(resolve, ms)),
-    onload: node => new Promise(resolve => {
-      const resolveFn = () => {
-        onload.delete(node, resolveFn)
-        resolve()
+      testBody.appendChild(testElementGroup)
+      const maybePromise = testFn(createTestHarness(t, testElement))
+      if (maybePromise && maybePromise.then) {
+        maybePromise.then(t.end).catch(t.error)
       }
-      node.isConnected ? resolveFn() : onload(node, resolveFn)
-    }),
-    unload: node => new Promise(resolve => {
-      const resolveFn = () => {
-        onload.delete(node, undefined, resolveFn)
-        resolve()
-      }
-      !node.isConnected ? resolveFn() : onload(node, undefined, resolveFn)
-    }),
-    raf: () => new Promise(resolve => window.requestAnimationFrame(() => resolve())),
-    click: (stringOrElement) => new Promise((resolve, reject) => {
-      if (typeof stringOrElement === 'string') stringOrElement = element.querySelector(stringOrElement)
-      if (!(stringOrElement instanceof window.HTMLElement)) return reject(new Error('stringOrElement needs to be an instance of HTMLElement or a querySelector that resolves to a HTMLElement'))
-      const el = stringOrElement
-      el.click()
-      window.requestAnimationFrame(() => resolve())
     })
-  })
+  }
+
+  queueTest.delay = function (ms) {
+    return create(ms)
+  }
+
+  return queueTest
+
+  function createTestHarness (t, element) {
+    return Object.assign(t, {
+      element,
+      sleep: ms => new Promise((resolve) => setTimeout(resolve, ms)),
+      onload: node => new Promise(resolve => {
+        const resolveFn = () => {
+          onload.delete(node, resolveFn)
+          resolve()
+        }
+        node.isConnected ? resolveFn() : onload(node, resolveFn)
+      }),
+      unload: node => new Promise(resolve => {
+        const resolveFn = () => {
+          onload.delete(node, undefined, resolveFn)
+          resolve()
+        }
+        !node.isConnected ? resolveFn() : onload(node, undefined, resolveFn)
+      }),
+      raf: () => new Promise(resolve => window.requestAnimationFrame(() => resolve())),
+      delay () {
+        return delay ? t.sleep(delay) : t.raf()
+      },
+      click (stringOrElement) {
+        toElement(stringOrElement).click()
+        return t.delay()
+      },
+      focus (stringOrElement) {
+        toElement(stringOrElement).focus()
+        return t.delay()
+      },
+      async type (str, event) {
+        for (const c of str.split('')) {
+          const el = t.element.querySelector(':focus')
+          if (!el) return
+          await t.delay()
+          el.dispatchEvent(new KeyboardEvent(event || 'keydown', {key: c}))
+        }
+        return t.delay()
+      }
+    })
+
+    function toElement (stringOrElement) {
+      if (typeof stringOrElement === 'string') stringOrElement = element.querySelector(stringOrElement)
+      if (!(stringOrElement instanceof window.HTMLElement)) throw new Error('stringOrElement needs to be an instance of HTMLElement or a querySelector that resolves to a HTMLElement')
+      return stringOrElement
+    }
+  }
+
 }
+
+module.exports = create(0)
